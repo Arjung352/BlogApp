1097ba3c27b975b386e0f8f416f8ef90dcc85c0b
Arjun gupta <rachnag352@gmail.com>
2024-09-06 14:00:54 +0530

    changing the backend from firebase to express

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./component/AboutMe/About";
 import Navbar from "./page/Navbar/Navbar";
 import Bloglist from "./component/Bloglist";
+function Maintaince() {
+  return (
+    <div className="h-screen bg-offWhite w-screen flex justify-center flex-col items-center">
+      <p className=" text-5xl font-salsa">Site under Maintaince</p>
+
+      <p className=" text-3xl font-salsa">Sorry for the inconvinince</p>
+    </div>
+  );
+}
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />}>
+        {/* <Route path="/" element={<Navbar />}>
           <Route index element={<Bloglist />} />
           <Route path="create" element={<Create />} />
           <Route path="aboutme" element={<About />} />
           <Route path="signin" element={<Signin />} />
           <Route path="show/:id" element={<ShowBlog />} />
           <Route path="edit-blog/:id" element={<EditBlog />} />
-        </Route>
-        <Route path="*" element={<div>404 Not Found</div>} />
+        </Route> */}
+        <Route path="*" element={<Maintaince />} />
       </Routes>
     </BrowserRouter>
   );
